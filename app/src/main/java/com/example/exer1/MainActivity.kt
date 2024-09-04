d44a4e04ddbca07e59e416753d786a54ef39140c
Charles <charles_juanillas@yahoo.com>
2024-09-04 09:45:22 +0800

    add to github

--- app/src/main/java/com/example/exer1/MainActivity.kt
+++ app/src/main/java/com/example/exer1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Exer1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Charles",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
